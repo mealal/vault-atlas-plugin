c8c36fd0415f2480272873f52d9f129658521a13
Alexey Menshikov <27022842+mealal@users.noreply.github.com>
2019-02-20 14:24:46 -0500

    Project created

---
+++ mongodb-atlas-plugin/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+	"github.com/hashicorp/vault/helper/pluginutil"
+	 "github.com/mealal/vault-atals-plugin"
+)
+
+func main() {
+	apiClientMeta := &pluginutil.APIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
+
+	err := atlas.Run(apiClientMeta.GetTLSConfig())
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
